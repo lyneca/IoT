4535131bd36d20f9bca4f39a1bdf8b02d2c3b480
wing2048 <atraguliauniliantauthr@gmail.com>
2015-10-21 10:24:14 +1100

    added dynamic sensor classes and options for fake data tests

--- grapher.py
+++ grapher.py
@@ -1,13 +1,13 @@
 __author__ = 'wing2048'
 from tkinter import *
 import threading
+import random
 
 import serial
 
 
-debug = False
-
-port = 'COM4'
+debug = True
+fake_data = True
 
 point_size = 1.5
 graph_x_offset = 48
@@ -25,26 +25,73 @@ grid_spacing_x = 30
 signal_delay = 100
 time_magnitude = 100
 root = Tk()
-root.wm_title('Readings from Arduino on ' + port)
+root.wm_title('Readings from sensors')
 root.configure(background='black')
 
 
-class SensorFrame:
-    def __init__(self, x, y):
+class Sensor:
+    def __init__(self, port, x, y, t='', dummy=fake_data):
         self.frame = Frame(root, width=frame_width, height=frame_height)
         self.frame.grid(column=x, row=y)
         self.temp = Canvas(self.frame, width=graph_width * 2 + 4, height=graph_height, bg='black')
         self.light = Canvas(self.frame, width=graph_width, height=graph_height, bg='black')
         self.sound = Canvas(self.frame, width=graph_width, height=graph_height, bg='black')
-        self.light.grid(row=0, column=0)
-        self.sound.grid(row=0, column=1)
-        self.temp.grid(row=1, columnspan=2)
+        self.title = Label(self.frame, text=t)
+        self.title.grid(row=0, columnspan=2)
+        self.light.grid(row=1, column=0)
+        self.sound.grid(row=1, column=1)
+        self.temp.grid(row=2, columnspan=2)
+
+        self.is_dummy = dummy
+
+        self.m_list = []
+        self.t_points = []
+        self.l_points = []
+        self.s_points = []
+        if not dummy:
+            self.serial = serial.Serial(port, 9600)
+        else:
+            self.thread = threading.Thread(target=self.read)
+
+    def read(self):
+        temp_list = []
+        if self.is_dummy:
+            m_list.append(Measurement(1, random.randint(300, 400), random.randint(15, 20), random.randint(30, 60)))
+        else:
+            while True:
+                read = self.serial.read()
+                if read == b'\r':
+                    break
+                temp_list.append(read.decode())
+            self.m_list.append(Measurement(*''.join(temp_list).strip().split()))
+        self.t_points = []
+        self.l_points = []
+        self.s_points = []
+        for m in m_list:
+            self.l_points.append(map(m.light, 0, 1000, 0, graph_height / grid_spacing_y - 3))
+            self.t_points.append(map(m.temp, 0, 45, 0, graph_height / grid_spacing_y - 3))
+            self.s_points.append(map(m.sound, 0, 100, 0, graph_height / grid_spacing_y - 3))
+            if len(self.t_points) > graph_width * 2 / grid_spacing_x - 1:
+                self.t_points.pop(0)
+            if len(self.l_points) > graph_width / grid_spacing_x - 1:
+                self.l_points.pop(0)
+            if len(self.s_points) > graph_width / grid_spacing_x - 1:
+                self.s_points.pop(0)
+        root.after(1000, self.start_thread)
+
+    def start_thread(self):
+        self.thread = threading.Thread(target=self.read)
+        self.thread.start()
 
 
-sensor_1 = SensorFrame(0, 0)
-sensor_2 = SensorFrame(0, 1)
-sensor_3 = SensorFrame(1, 0)
-sensor_4 = SensorFrame(1, 1)
+sensors = [
+    Sensor('COM1', 0, 0, "Computer Room"),
+    Sensor('COM2', 1, 0, "Office"),
+    Sensor('COM3', 2, 0, "A16"),
+    Sensor('COM4', 0, 1, "MMC"),
+    Sensor('COM5', 1, 1, "A1"),
+    Sensor('COM6', 2, 1, "B3"),
+]
 
 
 def map(v, fl, fh, tl, th):
@@ -123,36 +170,15 @@ def start_update_thread():
 
 
 def update():
-    temp_list = []
-    while True:
-        read = s.read()
-        if read == b'\r':
-            break
-        temp_list.append(read.decode())
-    m_list.append(Measurement(*''.join(temp_list).strip().split()))
-    with open('data.db', 'a') as file:
-        file.write(''.join(temp_list).strip() + '\n')
-    t_points = []
-    l_points = []
-    s_points = []
-    for m in m_list:
-        l_points.append(map(m.light, 0, 1000, 0, graph_height / grid_spacing_y - 3))
-        t_points.append(map(m.temp, 0, 45, 0, graph_height / grid_spacing_y - 3))
-        s_points.append(map(m.sound, 0, 100, 0, graph_height / grid_spacing_y - 3))
-        if len(t_points) > graph_width * 2 / grid_spacing_x - 1:
-            t_points.pop(0)
-        if len(l_points) > graph_width / grid_spacing_x - 1:
-            l_points.pop(0)
-        if len(s_points) > graph_width / grid_spacing_x - 1:
-            s_points.pop(0)
-    redraw(sensor_1.temp, t_points, 10, len(m_list), 'Temperature')
-    redraw(sensor_1.light, l_points, 1000 // (graph_height // grid_spacing_y), len(m_list), 'Light Level')
-    redraw(sensor_1.sound, s_points, 10, len(m_list), 'Sound level')
-    root.after(1, start_update_thread)
+    for sensor in sensors:
+        redraw(sensor.temp, sensor.t_points, 10, len(m_list), 'Temperature')
+        redraw(sensor.light, sensor.l_points, 1000 // (graph_height // grid_spacing_y), len(m_list), 'Light Level')
+        redraw(sensor.sound, sensor.s_points, 10, len(m_list), 'Sound level')
+    root.after(1000, start_update_thread)
 
 
 m_list = []
-if not debug:
-    s = serial.Serial(port, 9600)
-    root.after(1, start_update_thread)
+root.after(1, start_update_thread)
+for sensor in sensors:
+    root.after(1, sensor.start_thread)
 root.mainloop()
