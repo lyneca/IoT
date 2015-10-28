192543766490c297d0dee702ab7a2d0a58155f04
wing2048 <atraguliauniliantauthr@gmail.com>
2015-10-28 11:35:35 +1100

    built to exe

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import py2exe
+setup(
+    console=['grapher.py'],
+    name='IoT',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='wing2048',
+    author_email='lukegithub@gmail.com',
+    description=''
+)
