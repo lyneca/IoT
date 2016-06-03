fe4c6b98990037fbd1d6faf98472e2a9142107ad
lyneca <lukegithub@gmail.com>
2016-06-03 13:00:14 +1000

    added debug prints to tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,16 @@
 import gui
 
+gui.args.verbose = True
+gui.args.debug = True
 # Creates a sensor at ip:port
+gui.debug("Creating test sensor", -1)
 ip = "192.168.0.15"
 port = 80
 test_sensor = gui.Sensor(ip, port, "TestSensor", "#55FF55")
 
 # Grab some data from the sensor - if this works, the sensor will flash orange, then briefly blue.
+gui.debug("Reading data from test sensor.", -1)
+gui.debug("Sensor should flash orange, then briefly blue.", -1)
 content = test_sensor.read()
-print(content)
+gui.debug("Received data:", -1)
+gui.debug(content, -1)
